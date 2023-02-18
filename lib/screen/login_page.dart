77f188cc2edd44a231089189ceb1e35940bb28d5
desktop yoochanhong <chanhongy6@naver.com>
2023-02-19 04:18:46 +0900

    stf 위젯으로 변경

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   bool isClicked = false;
 
   @override
