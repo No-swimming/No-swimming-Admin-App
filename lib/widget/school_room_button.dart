14932c21f374cd223e864bb9756e1b000e476550
desktop yoochanhong <chanhongy6@naver.com>
2023-02-20 02:00:12 +0900

    학교 반 버튼 추가

---
+++ lib/widget/school_room_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SchoolRoomButton extends StatefulWidget {
+  const SchoolRoomButton({Key? key}) : super(key: key);
+
+  @override
+  State<SchoolRoomButton> createState() => _SchoolRoomButtonState();
+}
+
+class _SchoolRoomButtonState extends State<SchoolRoomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
