d24c1d4a16a3e167c1a90c56026aebf2c148e1b4
desktop yoochanhong <chanhongy6@naver.com>
2023-02-19 15:00:09 +0900

    학생 관리 텍스트 추가

--- lib/screen/student_management_page.dart
+++ lib/screen/student_management_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StudentManagementPage extends StatelessWidget {
   const StudentManagementPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 70.0.h, left: 20.0.w),
+            child: Text(
+              '학생 관리',
+              style: TextStyle(fontFamily: 'LINE Seed Sans KR',
+              fontSize: 30.0.sp,
+              fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
