10d24f79f1b3041df4fd65916a6a77a384ce9794
desktop yoochanhong <chanhongy6@naver.com>
2023-02-20 02:02:58 +0900

    반 버튼 클래스 완☆성

--- lib/widget/school_room_button.dart
+++ lib/widget/school_room_button.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:no_swimming_admin_app/provider/school_list_provider.dart';
+import 'package:provider/provider.dart';
 
 class SchoolRoomButton extends StatefulWidget {
-  const SchoolRoomButton({Key? key}) : super(key: key);
+  SchoolRoomButton({Key? key, required this.category}) : super(key: key);
+
+  String category;
 
   @override
   State<SchoolRoomButton> createState() => _SchoolRoomButtonState();
@@ -10,6 +15,52 @@ class SchoolRoomButton extends StatefulWidget {
 class _SchoolRoomButtonState extends State<SchoolRoomButton> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SchoolListProvider schoolListProvider =
+        Provider.of<SchoolListProvider>(context, listen: true);
+
+    return InkWell(
+      onTap: () {
+        setState(() {
+          schoolListProvider.emptyGrageList();
+          schoolListProvider.addSelectedGradeList(widget.category);
+        });
+      },
+      child: Container(
+        width: 50.0.w,
+        height: 38.0.h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(57)),
+          border: Border.all(
+              width: schoolListProvider.selectedGrade.contains(widget.category)
+                  ? 2
+                  : 1,
+              color: schoolListProvider.selectedGrade.contains(widget.category)
+                  ? Colors.black
+                  : Colors.black12),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                widget.category.toString(),
+                style: TextStyle(
+                    fontFamily: 'LINE Seed Sans KR',
+                    color: schoolListProvider.selectedGrade
+                            .contains(widget.category)
+                        ? Colors.black
+                        : const Color(0xff7F7F7F),
+                    fontSize: 16.0.sp,
+                    fontWeight: schoolListProvider.selectedGrade
+                            .contains(widget.category)
+                        ? FontWeight.bold
+                        : FontWeight.normal),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
