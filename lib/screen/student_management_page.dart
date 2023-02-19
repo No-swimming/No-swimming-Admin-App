cc312f187e321ee73a23d85896ce98a1995969fd
desktop yoochanhong <chanhongy6@naver.com>
2023-02-19 22:07:30 +0900

    학년 구분 버튼 초기 디자인

--- lib/screen/student_management_page.dart
+++ lib/screen/student_management_page.dart
@@ -14,19 +14,34 @@ class StudentManagementPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.only(top: 70.0.h, left: 20.0.w, bottom: 12.0.h),
-            child: Text(
-              '학생 관리',
-              style: TextStyle(
-                  fontFamily: 'LINE Seed Sans KR',
-                  fontSize: 30.0.sp,
-                  fontWeight: FontWeight.bold),
-            ),
+          Text(
+            '학생 관리',
+            style: TextStyle(
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 30.0.sp,
+                fontWeight: FontWeight.bold),
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(),
+              Container(
+                width: 68.0.w,
+                height: 38.0.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(57)),
+                  border: Border.all(width: 1, color: Colors.black12),
+                ),
+                child: Center(
+                    child: Text(
+                  '1학년',
+                  style: TextStyle(
+                    fontFamily: 'LINE Seed Sans KR',
+                    color: Color(0xff7F7F7F),
+                    fontSize: 16.0.sp,
+                  ),
+                )),
+              ),
             ],
           ),
         ],
