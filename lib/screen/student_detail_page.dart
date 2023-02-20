cab56934d5e12442ca318a91bce6da5c19e4d240
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 00:38:44 +0900

    틀만 잡아놓기

--- lib/screen/student_detail_page.dart
+++ lib/screen/student_detail_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StudentDetailPage extends StatelessWidget {
   const StudentDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Text('주영재(3602)'),
+          Text('작성한 독서록 8건'),
+          Divider(),
+          SizedBox(
+            width: 380.0.w,
+            height: 655.0.h,
+            child: ListView(),
+          )
+        ],
+      ),
+    );
   }
 }
