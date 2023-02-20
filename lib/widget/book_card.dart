36e7efffb63d4eee6cc3527829475febd6df4e44
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 02:36:24 +0900

    책 카드 위젯 얼추 완☆성

--- lib/widget/book_card.dart
+++ lib/widget/book_card.dart
@@ -7,36 +7,43 @@ class BookCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xffF2F2F2),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        color: Color(0xffF2F2F2),
+      ),
       width: 362.0.w,
       height: 68.0.h,
-      child: Row(
-        children: [
-          Image.asset('assets/images/book.png', width: 41.0.w, height: 60.0.h),
-          Padding(
-            padding: EdgeInsets.only(top: 9.0.h, left: 10.0.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '프로젝트 헤일메리',
-                  style: TextStyle(
+      child: Padding(
+        padding: EdgeInsets.all(4.0.r),
+        child: Row(
+          children: [
+            Image.asset('assets/images/book.png',
+                width: 41.0.w, height: 60.0.h),
+            Padding(
+              padding: EdgeInsets.only(top: 9.0.h, left: 10.0.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '프로젝트 헤일메리',
+                    style: TextStyle(
+                        fontSize: 16.0.sp,
+                        fontFamily: 'LINE Seed Sans KR',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '앤디 위어 | 2021',
+                    style: TextStyle(
                       fontSize: 16.0.sp,
                       fontFamily: 'LINE Seed Sans KR',
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '앤디 위어 | 2021',
-                  style: TextStyle(
-                    fontSize: 16.0.sp,
-                    fontFamily: 'LINE Seed Sans KR',
-                    color: Color(0xff7F7F7F),
+                      color: Color(0xff7F7F7F),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
