8d57eb287e950ea2b0ceb6947c9b43edef1d3576
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 00:36:04 +0900

    학생 상세 페이지 생성

---
+++ lib/screen/student_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StudentDetailPage extends StatelessWidget {
+  const StudentDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
