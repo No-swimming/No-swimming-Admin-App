521cf68397c2cede90366897c8b8522efde359e7
desktop yoochanhong <chanhongy6@naver.com>
2023-02-20 19:11:05 +0900

    서버 연동하면 데이터 넣으면 될듯?

--- lib/widget/student_card.dart
+++ lib/widget/student_card.dart
@@ -8,34 +8,37 @@ class StudentCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 380.0.w,
-      height: 58.0.h,
+      height: 65.0.h,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: const BorderRadius.all(Radius.circular(8)),
         border: Border.all(color: Color(0xffF2F2F2)),
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset('assets/images/noSwimmingIcon.png',
-              width: 42.0.w, height: 42.0.h),
-          SizedBox(width: 8.0.w),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('주영재',
+      child: Padding(
+        padding: EdgeInsets.all(8.0.r),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/noSwimmingIcon.png',
+                width: 42.0.w, height: 42.0.h),
+            SizedBox(width: 8.0.w),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('주영재',
+                    style: TextStyle(
+                        fontFamily: 'LINE Seed Sans KR',
+                        fontWeight: FontWeight.bold)),
+                Text(
+                  '3학년 6반 2번',
                   style: TextStyle(
                       fontFamily: 'LINE Seed Sans KR',
-                      fontWeight: FontWeight.bold)),
-              Text(
-                '3학년 6반 2번',
-                style: TextStyle(
-                    fontFamily: 'LINE Seed Sans KR',
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ],
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
