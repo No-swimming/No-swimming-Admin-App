c4f4795b7a252506aaee1d703e2f2984f288fc89
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 02:37:39 +0900

    독서록 카드 위젯 생성

---
+++ lib/widget/reading_journal_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReadingJournalCard extends StatelessWidget {
+  const ReadingJournalCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
