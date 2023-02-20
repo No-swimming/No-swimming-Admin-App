e49dacbc7cff3836b9fda532aabec545c784a171
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 00:43:30 +0900

    학생 상세 페이지로 네비게이터 연결

--- lib/widget/student_card.dart
+++ lib/widget/student_card.dart
@@ -1,43 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:no_swimming_admin_app/screen/student_detail_page.dart';
 
 class StudentCard extends StatelessWidget {
   const StudentCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 380.0.w,
-      height: 65.0.h,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
-        border: Border.all(color: Color(0xffF2F2F2)),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(8.0.r),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/images/noSwimmingIcon.png',
-                width: 42.0.w, height: 42.0.h),
-            SizedBox(width: 8.0.w),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('주영재',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => StudentDetailPage()),
+        );
+      },
+      child: Container(
+        width: 380.0.w,
+        height: 65.0.h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          border: Border.all(color: Color(0xffF2F2F2)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0.r),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/images/noSwimmingIcon.png',
+                  width: 42.0.w, height: 42.0.h),
+              SizedBox(width: 8.0.w),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('주영재',
+                      style: TextStyle(
+                          fontFamily: 'LINE Seed Sans KR',
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    '3학년 6반 2번',
                     style: TextStyle(
                         fontFamily: 'LINE Seed Sans KR',
-                        fontWeight: FontWeight.bold)),
-                Text(
-                  '3학년 6반 2번',
-                  style: TextStyle(
-                      fontFamily: 'LINE Seed Sans KR',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ],
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
