ba24f82328a09956448a1ec2c77b27c66f9aedf1
desktop yoochanhong <chanhongy6@naver.com>
2023-02-20 18:56:19 +0900

    기본적인 틀 만들기

--- lib/widget/student_card.dart
+++ lib/widget/student_card.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StudentCard extends StatelessWidget {
   const StudentCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: 380.0.w,
+      height: 58.0.h,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset('assets/images/noSwimmingIcon.png',
+              width: 42.0.w, height: 42.0.h),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('주영재'),
+              Text('3학년 6반 2번'),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
