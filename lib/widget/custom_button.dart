c34907bddaccaa896a6f85e2e994923c8f9a7bea
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 23:37:30 +0900

    커스텀 버튼 위젯 완성

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:no_swimming_admin_app/widget/check_popup_card.dart';
 
 class CustomButtom extends StatelessWidget {
-  const CustomButtom({Key? key}) : super(key: key);
+  CustomButtom(
+      {Key? key,
+      required this.buttonText,
+      required this.width,
+      required this.height,
+      required this.backgroundColor,
+      required this.fontSize,
+      required this.textColor})
+      : super(key: key);
+
+  String buttonText;
+  double width;
+  double height;
+  double fontSize;
+  Color backgroundColor;
+  Color textColor;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: width.w,
+      height: height.h,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            elevation: 0.0,
+            backgroundColor: backgroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(99),
+            )),
+        onPressed: () {},
+        child: Text(
+          buttonText,
+          style:
+              TextStyle(fontFamily: 'LINE Seed Sans KR', fontSize: fontSize.sp),
+        ),
+      ),
+    );
   }
 }
