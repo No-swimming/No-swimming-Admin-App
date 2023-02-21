2d09b789a88b810c59022199518eb05eabb51274
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 16:32:35 +0900

    내부 위젯 패딩값 16.0.r

--- lib/widget/check_popup_card.dart
+++ lib/widget/check_popup_card.dart
@@ -7,30 +7,33 @@ void checkPopupCard(context, title, bodyText) {
     builder: (BuildContext context) => AlertDialog(
       insetPadding: EdgeInsets.only(left: 24.0.w, right: 24.0.w),
       //AlertDialog에 있는 디폴트 패딩값 제거
-      content: SizedBox(
-        width: 372.0.w,
-        height: 155.0.h,
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28.0.sp,
-                  fontFamily: 'LINE Seed Sans KR'),
-            ),
-            Text(
-              bodyText,
-              style:
-                  TextStyle(fontSize: 16.0.sp, fontFamily: 'LINE Seed Sans KR'),
-            ),
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text('취소')),
-                ElevatedButton(onPressed: () {}, child: Text('확인')),
-              ],
-            )
-          ],
+      content: Padding(
+        padding: EdgeInsets.all(16.0.r),
+        child: SizedBox(
+          width: 372.0.w,
+          height: 155.0.h,
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28.0.sp,
+                    fontFamily: 'LINE Seed Sans KR'),
+              ),
+              Text(
+                bodyText,
+                style:
+                    TextStyle(fontSize: 16.0.sp, fontFamily: 'LINE Seed Sans KR'),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('취소')),
+                  ElevatedButton(onPressed: () {}, child: Text('확인')),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ),
