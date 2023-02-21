2997e35ee91b554bfa9ae14aedf79178f199d1ce
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 16:48:41 +0900

    팝업 창 구도 잡아놓기

--- lib/widget/check_popup_card.dart
+++ lib/widget/check_popup_card.dart
@@ -21,15 +21,53 @@ void checkPopupCard(context, title, bodyText) {
                     fontSize: 28.0.sp,
                     fontFamily: 'LINE Seed Sans KR'),
               ),
+              SizedBox(height: 16.0.h),
               Text(
                 bodyText,
-                style:
-                    TextStyle(fontSize: 16.0.sp, fontFamily: 'LINE Seed Sans KR'),
+                style: TextStyle(
+                    fontSize: 16.0.sp, fontFamily: 'LINE Seed Sans KR'),
               ),
+              SizedBox(height: 16.0.h),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text('취소')),
-                  ElevatedButton(onPressed: () {}, child: Text('확인')),
+                  SizedBox(
+                    width: 140.0.w,
+                    height: 33.0.h,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          backgroundColor: Color(0xffF2F2F2),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(99),
+                          )),
+                      onPressed: () {},
+                      child: Text(
+                        '취소',
+                        style:
+                        TextStyle(fontFamily: 'LINE Seed Sans KR', fontSize: 16.0.sp, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 5.0.w),
+                  SizedBox(
+                    width: 140.0.w,
+                    height: 33.0.h,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(99),
+                          )),
+                      onPressed: () {},
+                      child: Text(
+                        '확인',
+                        style:
+                        TextStyle(fontFamily: 'LINE Seed Sans KR', fontSize: 16.0.sp),
+                      ),
+                    ),
+                  ),
                 ],
               )
             ],
