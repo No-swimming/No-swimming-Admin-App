63726b659e0800b67454a06fcdaaefce659de17a
desktop yoochanhong <chanhongy6@naver.com>
2023-02-21 15:01:08 +0900

    팝업창 틀만 잡아놓기

--- lib/widget/check_popup_card.dart
+++ lib/widget/check_popup_card.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CheckPopupCard extends StatelessWidget {
-  const CheckPopupCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+void checkPopupCard(context, title, bodyText) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return Dialog(
+        child: Container(
+          width: 372.0.w,
+          height: 155.0.h,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              Text('피드백 전송'),
+              Text('ㅈㅅ ㄱㄱ?'),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('취소')),
+                  ElevatedButton(onPressed: () {}, child: Text('확인')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
