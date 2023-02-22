cc7943a0a4dd8727eeaf6c2a8d5aebbad91e0600
desktop yoochanhong <chanhongy6@naver.com>
2023-02-22 16:59:57 +0900

    일단 폰트, 글꼴, 크기 잡아놓음

--- lib/screen/review_page.dart
+++ lib/screen/review_page.dart
@@ -13,23 +13,63 @@ class ReviewPage extends StatelessWidget {
         padding: EdgeInsets.only(top: 70.0.h, left: 20.0.w, right: 20.0.w),
         child: Column(
           children: [
-            Text('독서록 확인'),
-            Text('선택한 책'),
+            Text(
+              '독서록 확인',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 30.0.sp,
+              ),
+            ),
+            Text(
+              '선택한 책',
+              style: TextStyle(
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 16.0.sp,
+                color: Color(0xff7F7F7F),
+              ),
+            ),
             BookCard(),
-            Text('담당 선생님'),
-            Text('박생선(saengSun88'),
+            Text(
+              '담당 선생님',
+              style: TextStyle(
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 16.0.sp,
+                color: Color(0xff7F7F7F),
+              ),
+            ),
+            Text(
+              '박생선(saengSun88)',
+              style: TextStyle(
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 20.0.sp,
+              ),
+            ),
             const Divider(
               thickness: 1,
               color: Color(0xffBFBFBF),
             ),
-            Text('나의 라임'),
+            Text(
+              '나의 라임',
+              style: TextStyle(
+                  fontFamily: 'LINE Seed Sans KR',
+                  fontSize: 24.0.sp,
+                  fontWeight: FontWeight.bold),
+            ),
             Text('대충 학생이 써놓은 글'),
             Text('7단어 44글자 공백 제외 36글자'),
             const Divider(
               thickness: 1,
               color: Color(0xffBFBFBF),
             ),
-            Text('피드백'),
+            Text(
+              '피드백',
+              style: TextStyle(
+                fontFamily: 'LINE Seed Sans KR',
+                fontSize: 16.0.sp,
+                color: Color(0xff7F7F7F),
+              ),
+            ),
             Text('감동적이다'),
           ],
         ),
