447c48be310f5b1be02b09b8a2a9c31749d1f15d
desktop yoochanhong <chanhongy6@naver.com>
2023-02-22 16:39:16 +0900

    확인 버튼도 함수 실행 후 팝업창 제거

--- lib/widget/check_popup_card.dart
+++ lib/widget/check_popup_card.dart
@@ -5,61 +5,65 @@ import 'package:no_swimming_admin_app/widget/custom_button.dart';
 void checkPopupCard(context, title, bodyText, Function func) {
   showDialog(
     context: context,
-    builder: (BuildContext context) => Dialog(
-      insetPadding: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      backgroundColor: Colors.white,
-      child: Container(
-        padding: EdgeInsets.all(16.0.r),
-        width: 372.0.w,
-        height: 170.0.h,
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                  fontSize: 28.0.sp,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'LINE Seed Sans KR'),
-            ),
-            SizedBox(height: 16.0.h),
-            Text(
-              bodyText,
-              style: TextStyle(
-                fontSize: 16.0.sp,
-                fontFamily: 'LINE Seed Sans KR',
-              ),
-            ),
-            SizedBox(height: 16.0.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    builder: (BuildContext context) =>
+        Dialog(
+          insetPadding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          backgroundColor: Colors.white,
+          child: Container(
+            padding: EdgeInsets.all(16.0.r),
+            width: 372.0.w,
+            height: 170.0.h,
+            child: Column(
               children: [
-                CustomButtom(
-                  buttonText: '취소',
-                  width: 166.0,
-                  height: 33.0,
-                  backgroundColor: Color(0xffF2F2F2),
-                  fontSize: 16.0,
-                  textColor: Colors.black,
-                  func: () => Navigator.pop(context),
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontSize: 28.0.sp,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'LINE Seed Sans KR'),
                 ),
-                SizedBox(width: 8.0.w),
-                CustomButtom(
-                  buttonText: '확인',
-                  width: 166.0,
-                  height: 33.0,
-                  backgroundColor: Colors.black,
-                  fontSize: 16.0,
-                  textColor: Colors.white,
-                  func: () => func(),
+                SizedBox(height: 16.0.h),
+                Text(
+                  bodyText,
+                  style: TextStyle(
+                    fontSize: 16.0.sp,
+                    fontFamily: 'LINE Seed Sans KR',
+                  ),
+                ),
+                SizedBox(height: 16.0.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomButtom(
+                      buttonText: '취소',
+                      width: 166.0,
+                      height: 33.0,
+                      backgroundColor: Color(0xffF2F2F2),
+                      fontSize: 16.0,
+                      textColor: Colors.black,
+                      func: () => Navigator.pop(context),
+                    ),
+                    SizedBox(width: 8.0.w),
+                    CustomButtom(
+                      buttonText: '확인',
+                      width: 166.0,
+                      height: 33.0,
+                      backgroundColor: Colors.black,
+                      fontSize: 16.0,
+                      textColor: Colors.white,
+                      func: () {
+                        func();
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
-    ),
   );
 }
