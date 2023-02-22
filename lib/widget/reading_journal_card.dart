e06542c334ca81da95e7b41b77c2ef9d5b0248d8
desktop yoochanhong <chanhongy6@naver.com>
2023-02-22 15:43:10 +0900

    독서록 카드 리팩토링

--- lib/widget/reading_journal_card.dart
+++ lib/widget/reading_journal_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:no_swimming_admin_app/widget/book_card.dart';
+import 'package:no_swimming_admin_app/widget/custom_button.dart';
 
 class ReadingJournalCard extends StatelessWidget {
   const ReadingJournalCard({Key? key}) : super(key: key);
@@ -56,44 +57,22 @@ class ReadingJournalCard extends StatelessWidget {
             SizedBox(height: 8.0.h),
             Row(
               children: [
-                SizedBox(
-                  width: 85.0.w,
-                  height: 33.0.h,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        elevation: 0.0,
-                        backgroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(99),
-                        )),
-                    onPressed: () {},
-                    child: Text(
-                      '내용 열기',
-                      style: TextStyle(
-                          fontFamily: 'LINE Seed Sans KR', fontSize: 13.0.sp),
-                    ),
-                  ),
+                CustomButtom(
+                  buttonText: '내용 열기',
+                  width: 90.0,
+                  height: 33.0,
+                  backgroundColor: Colors.black,
+                  fontSize: 14.0,
+                  textColor: Colors.white,
                 ),
                 SizedBox(width: 8.0.w),
-                SizedBox(
-                  width: 113.0.w,
-                  height: 33.0.h,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        elevation: 0.0,
-                        backgroundColor: Color(0xffF2F2F2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(99),
-                        )),
-                    onPressed: () {},
-                    child: Text(
-                      '마감으로 표시',
-                      style: TextStyle(
-                          fontFamily: 'LINE Seed Sans KR',
-                          fontSize: 14.0.sp,
-                          color: Colors.black),
-                    ),
-                  ),
+                CustomButtom(
+                  buttonText: '마감으로 표시',
+                  width: 113.0,
+                  height: 33.0,
+                  backgroundColor: Color(0xffF2F2F2),
+                  fontSize: 14.0,
+                  textColor: Colors.black,
                 ),
               ],
             ),
