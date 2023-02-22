66c5e925619c9143d79b496a02311ef51f38c454
desktop yoochanhong <chanhongy6@naver.com>
2023-02-22 22:05:29 +0900

    로고, 로그인 글씨 추가

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -18,100 +18,128 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.only(top: 258.0.h, left: 20.0.w, right: 20.0.w),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              width: 380.0.w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    ' 아이디',
-                    style: TextStyle(
-                      fontFamily: 'LINE Seed Sans KR',
-                      color: Color(0xff7F7F7F),
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/appLogo.png',
+                  width: 118.71.w,
+                  height: 29.0.h,
+                ),
+                Text(
+                  '로그인',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'LINE Seed Sans KR',
+                    fontSize: 30.0.sp,
                   ),
-                  SizedBox(height: 10.0.h),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            SizedBox(height: 32.0.h),
-            SizedBox(
-              width: 380.0.w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    ' 비밀번호',
-                    style: TextStyle(
-                      fontFamily: 'LINE Seed Sans KR',
-                      color: Color(0xff7F7F7F),
-                    ),
-                  ),
-                  SizedBox(height: 10.0.h),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      border: Border.all(width: 1, color: Colors.black12),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isClicked ? isClicked = false : isClicked = true;
-                            });
-                          },
-                          icon: isClicked
-                              ? const Icon(FluentIcons.eye_24_regular)
-                              : const Icon(FluentIcons.eye_off_24_regular),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 380.0.w,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        ' 아이디',
+                        style: TextStyle(
+                          fontFamily: 'LINE Seed Sans KR',
+                          color: Color(0xff7F7F7F),
                         ),
                       ),
-                    ),
+                      SizedBox(height: 10.0.h),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(width: 1, color: Colors.black12),
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 32.0.h),
-            CustomButtom(
-              buttonText: '로그인',
-              width: 380.0,
-              height: 35.0,
-              backgroundColor: Colors.black,
-              fontSize: 17.0,
-              textColor: Colors.white,
-              func: () => Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => StudentManagementPage()),
-                  (route) => false),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 32.0),
-              child: Center(
-                  child: Text(
-                "계정을 발급받으려면 관리자에게 문의하십시오",
-                style: TextStyle(
-                  fontFamily: 'LINE Seed Sans KR',
                 ),
-              )),
+                SizedBox(height: 32.0.h),
+                SizedBox(
+                  width: 380.0.w,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        ' 비밀번호',
+                        style: TextStyle(
+                          fontFamily: 'LINE Seed Sans KR',
+                          color: Color(0xff7F7F7F),
+                        ),
+                      ),
+                      SizedBox(height: 10.0.h),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(width: 1, color: Colors.black12),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isClicked
+                                      ? isClicked = false
+                                      : isClicked = true;
+                                });
+                              },
+                              icon: isClicked
+                                  ? const Icon(FluentIcons.eye_24_regular)
+                                  : const Icon(FluentIcons.eye_off_24_regular),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 32.0.h),
+                CustomButtom(
+                  buttonText: '로그인',
+                  width: 380.0,
+                  height: 35.0,
+                  backgroundColor: Colors.black,
+                  fontSize: 17.0,
+                  textColor: Colors.white,
+                  func: () => Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => StudentManagementPage()),
+                      (route) => false),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 32.0),
+                  child: Center(
+                      child: Text(
+                    "계정을 발급받으려면 관리자에게 문의하십시오",
+                    style: TextStyle(
+                      fontFamily: 'LINE Seed Sans KR',
+                    ),
+                  )),
+                ),
+              ],
             ),
           ],
         ),
