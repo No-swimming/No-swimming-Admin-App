5b190b2d256f30583263036049abd310ad9819cb
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-21 09:41:31 +0900

    header 추가

--- lib/service/login.dart
+++ lib/service/login.dart
@@ -7,12 +7,14 @@ import 'package:no_swimming_admin_app/baseurl.dart';
 
 Future<LoginResponse> login(String email, String password) async {
   var deviceToken = await FirebaseMessaging.instance.getToken();
+  var data = {
+    "email": email,
+    "password": password,
+    "device_token": deviceToken
+  };
+  var body = json.encode(data);
   final response = await http.post(Uri.parse('$baseurl/teacher/device-token'),
-      body: {
-        "email": email,
-        "password": password,
-        "device_token": deviceToken
-      });
+      headers: {"Content-Type": "application/json"}, body: body);
   print(response.body);
   if (response.statusCode == 200) {
     return LoginResponse.fromJson(jsonDecode(response.body));
