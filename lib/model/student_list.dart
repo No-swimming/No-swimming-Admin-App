5e475ead1160ad534eb0c2d8314eb5646c8914c2
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-23 13:28:57 +0900

    학생 리스트 클래스 파일

---
+++ lib/model/student_list.dart
@@ -0,0 +1,24 @@
+import 'package:no_swimming_admin_app/model/student.dart';
+
+class StudentList {
+  List<Student>? studentList;
+
+  StudentList({this.studentList});
+
+  StudentList.fromJson(Map<String, dynamic> json) {
+    if (json['student_list'] != null) {
+      studentList = <Student>[];
+      json['student_list'].forEach((v) {
+        studentList!.add(Student.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (studentList != null) {
+      data['student_list'] = studentList!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
