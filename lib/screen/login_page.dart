1fa484385d57a7b664c737087fa8d58b61374af9
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-23 21:07:53 +0900

    버튼 클릭 시 비밀번호 가리기 기능 추가

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -108,6 +108,7 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                           child: TextField(
                             controller: passwordController,
+                            obscureText: isClicked ? true : false,
                             decoration: InputDecoration(
                               border: InputBorder.none,
                               suffixIcon: IconButton(
