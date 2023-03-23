2613bc671244da29d1ce00784832536be639e6dc
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-23 15:18:04 +0900

    토큰 제거

--- lib/service/get_student_list.dart
+++ lib/service/get_student_list.dart
@@ -3,13 +3,9 @@ import 'dart:convert';
 import 'package:no_swimming_admin_app/model/student_list.dart';
 import 'package:http/http.dart' as http;
 import 'package:no_swimming_admin_app/baseurl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<StudentList> getStudentList() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var token = prefs.getString('access_token');
-  final response = await http.get(Uri.parse('$baseurl/student/list'),
-      headers: {'Bearer Token': token.toString()});
+  final response = await http.get(Uri.parse('$baseurl/student/list'));
   print(response.body);
   if (response.statusCode == 200) {
     return StudentList.fromJson(jsonDecode(response.body));
