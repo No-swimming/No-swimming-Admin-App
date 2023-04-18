ee8cea0f24fb07714f91a91344243cde139260cf
yoochanhong <chanhongy6@naver.com>
2023-04-18 18:23:29 +0900

    쿼리에 grade 속성 넣어주기

--- lib/widget/school_year_button.dart
+++ lib/widget/school_year_button.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:no_swimming_admin_app/provider/school_list_provider.dart';
+import 'package:no_swimming_admin_app/service/get_student_list.dart';
 import 'package:provider/provider.dart';
 
 class SchoolYearButton extends StatefulWidget {
-  SchoolYearButton({Key? key, required this.category}) : super(key: key);
-  String category;
+  SchoolYearButton(
+      {Key? key, required this.categoryText, required this.categoryNum})
+      : super(key: key);
+  String categoryText;
+  int categoryNum;
 
   @override
   State<SchoolYearButton> createState() => _SchoolYearButtonState();
@@ -21,11 +25,13 @@ class _SchoolYearButtonState extends State<SchoolYearButton> {
       onTap: () {
         setState(() {
           schoolListProvider.emptyGrageList();
-          schoolListProvider.addSelectedGradeList(widget.category);
+          schoolListProvider.addSelectedGradeList(widget.categoryText);
+          schoolListProvider.studentList =
+              getStudentList(grade: widget.categoryNum);
         });
       },
       child: Container(
-        width: schoolListProvider.selectedGrade.contains(widget.category)
+        width: schoolListProvider.selectedGrade.contains(widget.categoryText)
             ? 118.0.w
             : 72.0.w,
         height: 38.0.h,
@@ -33,34 +39,37 @@ class _SchoolYearButtonState extends State<SchoolYearButton> {
           color: Colors.white,
           borderRadius: const BorderRadius.all(Radius.circular(57)),
           border: Border.all(
-              width: schoolListProvider.selectedGrade.contains(widget.category)
-                  ? 2
-                  : 1,
-              color: schoolListProvider.selectedGrade.contains(widget.category)
-                  ? Colors.black
-                  : Colors.black12),
+              width:
+                  schoolListProvider.selectedGrade.contains(widget.categoryText)
+                      ? 2
+                      : 1,
+              color:
+                  schoolListProvider.selectedGrade.contains(widget.categoryText)
+                      ? Colors.black
+                      : Colors.black12),
         ),
         child: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Text(
-                widget.category.toString(),
+                widget.categoryText,
                 style: TextStyle(
                     fontFamily: 'LINE Seed Sans KR',
                     color: schoolListProvider.selectedGrade
-                            .contains(widget.category)
+                            .contains(widget.categoryText)
                         ? Colors.black
                         : const Color(0xff7F7F7F),
                     fontSize: 16.0.sp,
                     fontWeight: schoolListProvider.selectedGrade
-                            .contains(widget.category)
+                            .contains(widget.categoryText)
                         ? FontWeight.bold
                         : FontWeight.normal),
               ),
-              if (schoolListProvider.selectedGrade.contains(widget.category))
+              if (schoolListProvider.selectedGrade
+                  .contains(widget.categoryText))
                 Text(
-                  '72명',
+                  "72명",
                   style: TextStyle(
                       fontFamily: 'LINE Seed Sans KR',
                       color: const Color(0xff7F7F7F),
