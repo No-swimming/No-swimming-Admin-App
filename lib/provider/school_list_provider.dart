26d31d24175a667aafe072f68967dfc4450db741
yoochanhong <chanhongy6@naver.com>
2023-04-18 18:23:04 +0900

    addStudentList 함수 추가

--- lib/provider/school_list_provider.dart
+++ lib/provider/school_list_provider.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:no_swimming_admin_app/model/student_list.dart';
+import 'package:no_swimming_admin_app/service/get_student_list.dart';
 
 class SchoolListProvider with ChangeNotifier {
   List<String> selectedGrade = List.empty(growable: true);
   List<String> selectedRoom = List.empty(growable: true);
   Future<StudentList>? studentList;
+
+  void addStudentList() {
+    studentList = getStudentList();
+    notifyListeners();
+  }
 
   void emptyGrageList() {
     selectedGrade = List.empty(growable: true);
