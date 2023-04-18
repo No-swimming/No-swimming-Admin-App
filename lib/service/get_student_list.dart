8f4ac084a2892510f88a571c39141565f03b3fbf
yoochanhong <chanhongy6@naver.com>
2023-04-18 16:50:33 +0900

    학생 리스트 호출 시 네임드 파라미터 적용

--- lib/service/get_student_list.dart
+++ lib/service/get_student_list.dart
@@ -4,12 +4,23 @@ import 'package:no_swimming_admin_app/model/student_list.dart';
 import 'package:http/http.dart' as http;
 import 'package:no_swimming_admin_app/baseurl.dart';
 
-Future<StudentList> getStudentList() async {
-  final response = await http.get(Uri.parse('$baseurl/student/list'));
-  if (response.statusCode == 200) {
-    print(utf8.decode(response.bodyBytes));
-    return StudentList.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
-  } else {
-    throw Exception('예외');
+class GetStudentList {
+  String url = "$baseurl/student/list";
+
+  Future<StudentList> getStudentList({int? grade, int? classNum}) async {
+    if (grade != null) {
+      if (classNum == null) {
+        url = "$url?grade=$grade";
+      } else {
+        url = "$url?grade=$grade&classNum=$classNum";
+      }
+    }
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print(utf8.decode(response.bodyBytes));
+      return StudentList.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+    } else {
+      throw Exception('예외');
+    }
   }
 }
