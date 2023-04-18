447ab51be48640233fd88570063e26e1d5e3ad79
yoochanhong <chanhongy6@naver.com>
2023-04-18 16:53:44 +0900

    뷰모델 클래스 제거

--- lib/service/get_student_list.dart
+++ lib/service/get_student_list.dart
@@ -4,23 +4,21 @@ import 'package:no_swimming_admin_app/model/student_list.dart';
 import 'package:http/http.dart' as http;
 import 'package:no_swimming_admin_app/baseurl.dart';
 
-class GetStudentList {
-  String url = "$baseurl/student/list";
+String url = "$baseurl/student/list";
 
-  Future<StudentList> getStudentList({int? grade, int? classNum}) async {
-    if (grade != null) {
-      if (classNum == null) {
-        url = "$url?grade=$grade";
-      } else {
-        url = "$url?grade=$grade&classNum=$classNum";
-      }
-    }
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      print(utf8.decode(response.bodyBytes));
-      return StudentList.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+Future<StudentList> getStudentList({int? grade, int? classNum}) async {
+  if (grade != null) {
+    if (classNum == null) {
+      url = "$url?grade=$grade";
     } else {
-      throw Exception('예외');
+      url = "$url?grade=$grade&classNum=$classNum";
     }
+  }
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    print(utf8.decode(response.bodyBytes));
+    return StudentList.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  } else {
+    throw Exception('예외');
   }
 }
