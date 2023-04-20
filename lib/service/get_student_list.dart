87d43b07838ef0848e0236da63996dbd9e7400b3
yoochanhong <chanhongy6@naver.com>
2023-04-20 16:31:14 +0900

    파라미터에 따른 변수 상태값 변경

--- lib/service/get_student_list.dart
+++ lib/service/get_student_list.dart
@@ -3,15 +3,24 @@ import 'dart:convert';
 import 'package:no_swimming_admin_app/model/student_list.dart';
 import 'package:http/http.dart' as http;
 import 'package:no_swimming_admin_app/baseurl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<StudentList> getStudentList({int? grade, int? classNum}) async {
   String url = "$baseurl/student/list";
+  int gradeNum, classNumNum;
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
   if (grade != null) {
     if (classNum == null) {
       url = "$url?grade=$grade";
+      classNumNum = 0;
+      gradeNum = grade;
     } else {
       url = "$url?grade=$grade&classNum=$classNum";
+      gradeNum = grade;
+      classNumNum = classNum;
     }
+  } else {
+    gradeNum = classNumNum = 0;
   }
   final response = await http.get(Uri.parse(url));
   if (response.statusCode == 200) {
