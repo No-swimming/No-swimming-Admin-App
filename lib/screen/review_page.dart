dd8140039e7f5b5b94d2edbfbb320bff80f5bca1
yoochanhong <chanhongy6@naver.com>
2023-04-21 13:28:10 +0900

    키보드 오버플로우 에러 해결

--- lib/screen/review_page.dart
+++ lib/screen/review_page.dart
@@ -11,122 +11,124 @@ class ReviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.only(top: 70.0.h, left: 20.0.w, right: 20.0.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '독서록 확인',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'LINE Seed Sans KR',
-                fontSize: 30.0.sp,
-              ),
-            ),
-            SizedBox(height: 22.0.h),
-            Text(
-              ' 선택한 책',
-              style: TextStyle(
-                fontFamily: 'LINE Seed Sans KR',
-                fontSize: 16.0.sp,
-                color: Color(0xff7F7F7F),
-              ),
-            ),
-            BookCard(),
-            SizedBox(height: 11.0.h),
-            Text(
-              ' 담당 선생님',
-              style: TextStyle(
-                fontFamily: 'LINE Seed Sans KR',
-                fontSize: 16.0.sp,
-                color: Color(0xff7F7F7F),
-              ),
-            ),
-            Text(
-              '박생선(saengSun88)',
-              style: TextStyle(
-                fontFamily: 'LINE Seed Sans KR',
-                fontSize: 20.0.sp,
-              ),
-            ),
-            const Divider(
-              thickness: 1,
-              color: Color(0xffBFBFBF),
-            ),
-            Text(
-              '나의 라임',
-              style: TextStyle(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 70.0.h, left: 20.0.w, right: 20.0.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '독서록 확인',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                   fontFamily: 'LINE Seed Sans KR',
-                  fontSize: 24.0.sp,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text('대충 학생이 써놓은 글'),
-            Text('7단어 44글자 공백 제외 36글자'),
-            const Divider(
-              thickness: 1,
-              color: Color(0xffBFBFBF),
-            ),
-            SizedBox(height: 20.0.h),
-            Text(
-              ' 피드백',
-              style: TextStyle(
-                fontFamily: 'LINE Seed Sans KR',
-                fontSize: 16.0.sp,
-                color: Color(0xff7F7F7F),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                border: Border.all(width: 1, color: Colors.black12),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
+                  fontSize: 30.0.sp,
                 ),
               ),
-            ),
-            SizedBox(height: 12.0.h),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomButtom(
-                  buttonText: '통과로 표시',
-                  width: 125.0,
-                  height: 42.0,
-                  backgroundColor: Colors.black,
-                  fontSize: 19.0,
-                  textColor: Colors.white,
-                  func: () => checkPopupCard(
-                      context, '통과로 표시', '독서록 상태를 통과로 표시할까요?', func),
+              SizedBox(height: 22.0.h),
+              Text(
+                ' 선택한 책',
+                style: TextStyle(
+                  fontFamily: 'LINE Seed Sans KR',
+                  fontSize: 16.0.sp,
+                  color: Color(0xff7F7F7F),
                 ),
-                SizedBox(width: 8.0.w),
-                CustomButtom(
-                  buttonText: '피드백 전송',
-                  width: 125.0,
-                  height: 42.0,
-                  backgroundColor: Color(0xffF2F2F2),
-                  fontSize: 19.0,
-                  textColor: Colors.black,
-                  func: () => checkPopupCard(
-                      context, '피드백 전송', '학생에게 피드백을 전송할까요?', func),
+              ),
+              BookCard(),
+              SizedBox(height: 11.0.h),
+              Text(
+                ' 담당 선생님',
+                style: TextStyle(
+                  fontFamily: 'LINE Seed Sans KR',
+                  fontSize: 16.0.sp,
+                  color: Color(0xff7F7F7F),
                 ),
-              ],
-            ),
-            SizedBox(height: 8.0.h),
-            CustomButtom(
-              buttonText: '마감으로 표시',
-              width: 143.0,
-              height: 42.0,
-              backgroundColor: Color(0xffF2F2F2),
-              fontSize: 19.0,
-              textColor: Colors.black,
-              func: () => checkPopupCard(
-                  context, '마감으로 표시', '마감으로 표시된 뒤에는 독서록을 수정할 수 없습니다.', func),
-            ),
-          ],
+              ),
+              Text(
+                '박생선(saengSun88)',
+                style: TextStyle(
+                  fontFamily: 'LINE Seed Sans KR',
+                  fontSize: 20.0.sp,
+                ),
+              ),
+              const Divider(
+                thickness: 1,
+                color: Color(0xffBFBFBF),
+              ),
+              Text(
+                '나의 라임',
+                style: TextStyle(
+                    fontFamily: 'LINE Seed Sans KR',
+                    fontSize: 24.0.sp,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text('대충 학생이 써놓은 글'),
+              Text('7단어 44글자 공백 제외 36글자'),
+              const Divider(
+                thickness: 1,
+                color: Color(0xffBFBFBF),
+              ),
+              SizedBox(height: 20.0.h),
+              Text(
+                ' 피드백',
+                style: TextStyle(
+                  fontFamily: 'LINE Seed Sans KR',
+                  fontSize: 16.0.sp,
+                  color: Color(0xff7F7F7F),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  border: Border.all(width: 1, color: Colors.black12),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 12.0.h),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomButtom(
+                    buttonText: '통과로 표시',
+                    width: 125.0,
+                    height: 42.0,
+                    backgroundColor: Colors.black,
+                    fontSize: 19.0,
+                    textColor: Colors.white,
+                    func: () => checkPopupCard(
+                        context, '통과로 표시', '독서록 상태를 통과로 표시할까요?', func),
+                  ),
+                  SizedBox(width: 8.0.w),
+                  CustomButtom(
+                    buttonText: '피드백 전송',
+                    width: 125.0,
+                    height: 42.0,
+                    backgroundColor: Color(0xffF2F2F2),
+                    fontSize: 19.0,
+                    textColor: Colors.black,
+                    func: () => checkPopupCard(
+                        context, '피드백 전송', '학생에게 피드백을 전송할까요?', func),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.0.h),
+              CustomButtom(
+                buttonText: '마감으로 표시',
+                width: 143.0,
+                height: 42.0,
+                backgroundColor: Color(0xffF2F2F2),
+                fontSize: 19.0,
+                textColor: Colors.black,
+                func: () => checkPopupCard(
+                    context, '마감으로 표시', '마감으로 표시된 뒤에는 독서록을 수정할 수 없습니다.', func),
+              ),
+            ],
+          ),
         ),
       ),
     );
