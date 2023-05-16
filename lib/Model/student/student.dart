1d15736c7de74c3d1fb6489171934827e1d227a5
yoochanhong <chanhongy6@naver.com>
2023-05-16 23:13:57 +0900

    profile 추가

--- lib/Model/student/student.dart
+++ lib/Model/student/student.dart
@@ -1,27 +1,36 @@
 class Student {
+  int? userId;
   int? grade;
   int? classNum;
   int? number;
-  int? userId;
   String? name;
+  int? profileNum;
 
-  Student({this.grade, this.classNum, this.number, this.userId, this.name});
+  Student(
+      {this.userId,
+      this.grade,
+      this.classNum,
+      this.number,
+      this.name,
+      this.profileNum});
 
   Student.fromJson(Map<String, dynamic> json) {
+    userId = json['user_id'];
     grade = json['grade'];
     classNum = json['class_num'];
     number = json['number'];
-    userId = json['user_id'];
     name = json['name'];
+    profileNum = json['profile_num'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
+    data['user_id'] = userId;
     data['grade'] = grade;
     data['class_num'] = classNum;
-    data['user_id'] = userId;
     data['number'] = number;
     data['name'] = name;
+    data['profile_num'] = profileNum;
     return data;
   }
 }
