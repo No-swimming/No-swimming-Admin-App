a14ba0622ab11d7c65614e7dbaf99ae4e13df1eb
yoochanhong <chanhongy6@naver.com>
2023-05-16 23:13:51 +0900

    멀티 프로바이더 주입

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:no_swimming_admin_app/ViewModel/search_journal_list_view_model.dart';
 import 'firebase_options.dart';
 import 'package:no_swimming_admin_app/ViewModel/student_list_view_model.dart';
 import 'package:no_swimming_admin_app/View/Screen/login_page.dart';
@@ -63,8 +64,12 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(420, 930),
       builder: (context, child) {
-        return ChangeNotifierProvider(
-          create: (context) => StudentListViewModel(),
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => StudentListViewModel()),
+            ChangeNotifierProvider(
+                create: (context) => SearchJournalListViewModel()),
+          ],
           child: const MaterialApp(
             debugShowCheckedModeBanner: false,
             home: LoginPage(),
