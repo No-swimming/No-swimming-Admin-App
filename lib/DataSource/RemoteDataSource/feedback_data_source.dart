cb952e474a16659a700b0b430a65ae1abb4e1186
yoochanhong <chanhongy6@naver.com>
2023-05-21 23:06:08 +0900

    헤더 추가

--- lib/DataSource/RemoteDataSource/feedback_data_source.dart
+++ lib/DataSource/RemoteDataSource/feedback_data_source.dart
@@ -8,8 +8,13 @@ class FeedbackDataSource {
       int readingJournalId, String feedback) async {
     final response = await http.post(
         Uri.parse("$baseurl/feedback/$readingJournalId"),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $testToken"
+        },
         body: jsonEncode({"content": feedback}));
     if (response.statusCode >= 400) {
+      print(response.statusCode);
       throw Exception(response.body);
     }
   }
