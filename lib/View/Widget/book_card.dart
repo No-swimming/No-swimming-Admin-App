5566e31b168a4ad286af6112ffb31cbe04f99a63
yoochanhong <chanhongy6@naver.com>
2023-05-27 12:24:58 +0900

    이미지 url을 bookCard에게 넘겨줌

--- lib/View/Widget/book_card.dart
+++ lib/View/Widget/book_card.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BookCard extends StatelessWidget {
-  BookCard({Key? key, required this.title}) : super(key: key);
+  const BookCard({Key? key, required this.title, required this.imgUrl})
+      : super(key: key);
 
-  String title;
+  final String title, imgUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,7 @@ class BookCard extends StatelessWidget {
         padding: EdgeInsets.all(4.0.r),
         child: Row(
           children: [
-            Image.asset('assets/images/book.png',
-                width: 41.0.w, height: 60.0.h),
+            Image.network(imgUrl, width: 41.0.w, height: 60.0.h),
             Padding(
               padding: EdgeInsets.only(top: 9.0.h, left: 10.0.w),
               child: Column(
