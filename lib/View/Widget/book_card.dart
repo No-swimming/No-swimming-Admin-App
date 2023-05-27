0ac1db76686ab950da775d6c3560db7669655251
yoochanhong <chanhongy6@naver.com>
2023-05-27 14:32:03 +0900

    인덱스를 가져와서 책 사진을 보여주도록  변경

--- lib/View/Widget/book_card.dart
+++ lib/View/Widget/book_card.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:no_swimming_admin_app/ViewModel/book_view_model.dart';
+import 'package:provider/provider.dart';
 
 class BookCard extends StatelessWidget {
-  const BookCard({Key? key, required this.title, required this.imgUrl})
+  const BookCard({Key? key, required this.title, required this.bookIndex})
       : super(key: key);
 
-  final String title, imgUrl;
+  final String title;
+  final int bookIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,11 @@ class BookCard extends StatelessWidget {
         padding: EdgeInsets.all(4.0.r),
         child: Row(
           children: [
-            Image.network(imgUrl, width: 41.0.w, height: 60.0.h),
+            Consumer<BookViewModel>(
+                builder: (context, value, child) => Image.network(
+                    value.book.items![bookIndex].image.toString(),
+                    width: 41.0.w,
+                    height: 60.0.h)),
             Padding(
               padding: EdgeInsets.only(top: 9.0.h, left: 10.0.w),
               child: Column(
